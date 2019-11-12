4019ada670769246cf135c6f74e6b66daa9d809e
Alex Trask <Sarlack_Work@mail.ru>
2019-11-12 16:52:48 +0300

    some preparing for development is done

---
+++ build/webpack.base.config.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = {
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist'),
+    assets: 'assets/'
+}
+
+module.exports = {
+    externals: {
+        paths: PATHS
+    },
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: `${PATHS.assets}js/[name].js`,
+        path: PATHS.dist,
+        publicPath: '/'
+    },
+    module: {
+        rules: [{
+            test: /\.js$|jsx/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
+        }, 
+        {
+            test: /\.css$|scss/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: { sourceMap: true }
+                }, 
+                {
+                    loader: 'postcss-loader',
+                    options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: { sourceMap: true }
+                }
+            ]
+        },
+        {
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]'
+            }
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}css/[name].css`
+        }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            template: `${PATHS.src}/index.html`,
+            filename: './index.html'
+        }),
+        new CopyWebpackPlugin([
+            { from: `${PATHS.src}/img`, to: `${PATHS.assets}img` }
+        ])
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+}
---
+++ src/components/Entry/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//import Main from '../main'
+
+const Entry = () => {
+    return (
+        <>
+            Тест 
+        </>
+    )
+}
+
+export default Entry
